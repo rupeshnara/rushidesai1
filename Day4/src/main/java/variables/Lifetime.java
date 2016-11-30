9cc4da32b1942e583ac5aedd12185ba42ff9d6e0
rushidesai1 <rushidesai1@gmail.com>
2016-11-30 00:20:57 -0600

    Day 4 and Day5

--- Day4/src/main/java/variables/Lifetime.java
+++ Day4/src/main/java/variables/Lifetime.java
@@ -13,7 +13,9 @@ public class Lifetime {
     private int age;
     int height;
     public int weight;
-    public static int count = 0;
+    public static int count = 0;    //no longer attached to object, instead it is attached to lifetime of the class itself.
+    //class is loaded in the memory, and when we want to create an object, template of
+    // class is looked at and object is created as per that template
 
 
     public int getAge() {
@@ -40,7 +42,8 @@ public class Lifetime {
         this.weight = weight;
     }
 
-    public Lifetime getInstance() {
+    public static Lifetime getInstance() {
+        count++;
         Lifetime lf = new Lifetime();
 
         return lf;
@@ -50,6 +53,53 @@ public class Lifetime {
     public void changeAgeLifetime(Lifetime lifetime, int a) {
         a = a + 10;
         lifetime.age = a;
+    }
+
+
+    /*
+    *           Memory
+    * ----------------------------------
+    *           Lifetime
+    * -------------------------------------
+    *
+    *
+    *       count
+    *
+    *
+    * -----------------------------------
+    * on start of createTempInstance()
+    *
+    *           lf 5000
+    *     age  0
+    *     height 0
+    *     weight  100
+    *
+    * on end of createTempInstance()        ---> lf is destryoed
+    *
+    * ----------------------------------
+    *
+    * ---------------------------------
+    * stack for createTempInstance
+    *
+    * lf = 5000     //destryoed when createTempInstance() is exited
+    *
+    * --------------------------------
+    *
+    * */
+
+    //anotherLf
+    public static void createTempInstance() {
+        //createTempInstance never uses the data of the object on which it was called
+
+        //anotherLf.age
+        //anotherLf.weight
+        //anotherLf.height
+
+        //this lf will always be different than the object on which this method is called
+        Lifetime lf = new Lifetime();
+        lf.weight = 100;
+
+        System.out.println(lf.toString());
     }
 
     //    @Override
