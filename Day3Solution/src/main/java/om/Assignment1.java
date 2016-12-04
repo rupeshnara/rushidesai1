1af6a3ecdc0f83b8f4669eef8af9fa9c84d80c9f
kothasujana <sujana.kotha1992@gmail.com>
2016-12-03 20:44:55 -0800

    day3

---
+++ Day3Solution/src/main/java/om/Assignment1.java
@@ -0,0 +1,79 @@
+package om;
+
+/**
+ * Created by Paul on 12/3/2016.
+ */
+public class Assignment1 {
+
+   public static void printAll(int[] a){
+        for (int i = 0; i < 100; ++i) {
+            a[i] = i;
+            System.out.print(a[i]+ " ");
+
+        }}
+    // if the number is divisible by 2 then it is even method
+    public static void printEven(int[] a){
+
+        for(int i=1; i <= a.length; i++){
+
+
+            if( i % 2 == 0){
+                System.out.print(i + " ");
+            }
+    }}
+
+    // if the number is not divisible by 2 then it is odd method
+    public static  void printOdd(int[] a) {
+            for (int i = 1; i <= a.length; i++) {
+
+
+                if (i % 2 == 1) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
+    void printCond(int[] a, boolean condition) {
+
+        if (condition == true) {
+
+            printEven(a);
+
+
+        } else {
+            printOdd(a);
+        }
+    }
+    public static void main(String[] args) {
+
+        int[] a;
+        a = new int[100];
+
+        System.out.println("prints all elements : ");
+        printAll(a);
+
+        System.out.println("\n prints all even  elements :");
+        printEven(a);
+
+        System.out.println("\n prints all odd  elements :");
+        printOdd(a);
+
+
+
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day3Solution/src/main/java/om/Stars.java
@@ -0,0 +1,9 @@
+package om;
+
+/**
+ * Created by Paul on 12/3/2016.
+ */
+public class Stars {
+
+
+}
