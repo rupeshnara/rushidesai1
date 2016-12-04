ab30d7e0167a586a3f13af85f7cfcde947e6eca1
kothasujana <sujana.kotha1992@gmail.com>
2016-12-03 21:05:11 -0800

    day3

--- Day3Solution/src/main/java/om/Stars.java
+++ Day3Solution/src/main/java/om/Stars.java
@@ -6,4 +6,25 @@ package om;
 public class Stars {
 
 
-}
+    public static void starssquare(int n){
+
+        int i, j;
+
+        for (i = 0; i < n ; i++) {
+            for (j = 0; j < n-1; j++) {
+                // output a single "*" here
+                System.out.print("*");
+            }
+            // output a new line here
+            System.out.println("*");
+        }
+
+    }
+
+    public static void main(String[] args){
+
+
+
+        System.out.println("print n by n stars: ");
+        starssquare(3);
+}}
