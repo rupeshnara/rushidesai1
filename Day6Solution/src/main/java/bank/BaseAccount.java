2dba3e16a71ec19dca497e61f0b53aa2e23a4921
rushidesai1 <rushidesai1@gmail.com>
2016-12-04 23:46:30 -0600

    Day8

---
+++ Day6Solution/src/main/java/bank/BaseAccount.java
@@ -0,0 +1,138 @@
+package bank;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by desair4 on 12/4/2016.
+ */
+public class BaseAccount {
+    protected double initialBalance;
+    protected Integer accountNumber;
+    private static int count = 0;   //Think of does it make sense to make this field protected or private it better?
+    private String accountHash;
+    static private MyList previousAccountNumbers = new MyList(null); //start with some initial value
+
+    private Date date = new Date();
+
+    /*public BaseAccount(){
+
+    }*/
+
+    // Constructor to be used when client provides only the initial balance
+    // Validate initial balance and generate a random unique account number
+    public BaseAccount(double initialBalance, String accountHash1) {
+        // Validation of initial balance
+        if (validateInitialBalance()) {
+            throw new RuntimeException("Initial balance must be more than zero");
+        }
+
+        // Generate unique account number
+        accountNumber = generateAccountNumber();
+        isDuplicateAccountNumber(accountNumber);    //ideally we wont do this if we assume that the random generator draws numbers from uniform distribution and the period of the generator is big enough (which incase of java period is 2^48, so pretty huge)
+        previousAccountNumbers.nextItem = new MyList(accountNumber);
+
+        count++;
+    }
+
+    // Constructor to be used when client provides both initial balance and account number
+    // Validate the initial balance and check if account number has 8 digits
+    public BaseAccount(double initialBalance, Integer accountNumberClient, String accountHash1) {
+        // Validation of initial balance
+        if (validateInitialBalance()) {
+            throw new RuntimeException("Initial balance must be more than zero");
+        }
+
+        // Check if account number has 8 digits
+        if (validateNumberOfDigits()) {
+            throw new RuntimeException("Account Number should have exactly 8 digits");
+        }
+
+        isDuplicateAccountNumber(accountNumberClient);
+        accountNumber = accountNumberClient;
+
+        count++;
+    }
+
+    // Check for duplicate account number
+    private void isDuplicateAccountNumber(Integer accountNumber) {
+        MyList nextItem = previousAccountNumbers.nextItem;
+        while (nextItem != null) {
+            if (nextItem.accountNumber.equals(accountNumber)) {
+                throw new RuntimeException("Account Number already exists. Please provide another account number");
+            }
+            nextItem = nextItem.nextItem;
+        }
+    }
+
+    // Check for number of digits in account number
+    private boolean validateNumberOfDigits() {
+        Integer temp = accountNumber;
+        int digits = 0;
+        while (temp >= 10) {
+            temp = temp / 10;
+            digits++;
+        }
+        return digits == 8;
+    }
+
+    // Validate initial balance
+    private boolean validateInitialBalance() {
+        return initialBalance <= 0;
+    }
+
+    private Integer generateAccountNumber() {
+        Random r = new Random();
+        accountNumber = r.nextInt((9999999 + 1000000) - 1);
+        return accountNumber;
+    }
+
+    static protected String generateAccountHash() {
+        Date d = new Date();
+        return Base64.getEncoder().encodeToString(d.toString().getBytes());
+    }
+
+    public double getInitialBalance() {
+        return initialBalance;
+    }
+
+    public void setInitialBalance(double initialBalance) {
+        this.initialBalance = initialBalance;
+    }
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public String getAccountHash() {
+        return accountHash;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseAccount{" +
+                "initialBalance=" + initialBalance +
+                ", accountNumber=" + accountNumber +
+                ", date=" + date +
+                '}';
+    }
+
+
+    static private class MyList {
+        MyList nextItem;
+        Integer accountNumber;   //your data
+
+        MyList(Integer accountNumber1) {
+            accountNumber = accountNumber1;
+        }
+    }
+}
---
+++ Day6Solution/src/main/java/driver/Test.java
@@ -0,0 +1,37 @@
+package driver;
+
+import child.SavingsAccount;
+
+import java.util.Date;
+
+/**
+ * Created by desair4 on 12/4/2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+      /*  BaseAccount sAccount=new SavingsAccount(1000,25);
+        BaseAccount cAccount=new CheckingAccount(2000);
+        sAccount.credit(2500.0);
+        System.out.println("Savings Account Credit Amount:: "+ sAccount);
+        sAccount.debit(4500.0);
+        System.out.println("The Savings Account Debit Amount:: "+sAccount);
+
+        System.out.println("Account Number ...... Savings Account  : " + sAccount.accountNumber);
+        System.out.println("Account Number ...... Checking Account  : " + cAccount.accountNumber);*/
+        
+        SavingsAccount savingsAccount = new SavingsAccount(12, 1233434);
+
+        String savingsAccountStringRepresantation = savingsAccount.toString();
+        System.out.println(savingsAccountStringRepresantation);
+
+
+        Date d = new Date();
+
+        String s = d.toString();
+        System.out.println(s);
+
+        // System.out.println("To encode it with base 64 :" +);
+    }
+}
+
+
