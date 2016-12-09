39af3a0954065bd5e61c63caad78ba651ef4aa85
harikrishna <chandana16554@mail.npu.edu>
2016-12-09 01:37:53 -0800

    new files solved

--- Day9Solution/src/main/java/banking/parent/BaseAccount.java
+++ Day9Solution/src/main/java/banking/parent/BaseAccount.java
@@ -3,5 +3,32 @@ package banking.parent;
 /**
  * Created by harik on 12/8/2016.
  */
-public class BaseAccount {
+
+import banking.interfaces.IAccount;
+
+
+public abstract class BaseAccount implements IAccount {
+    protected double initialBalance;
+    protected Integer accountNumber;
+    public static int count = 0;
+    private String accountHash;
+
+
+    public double getInitialBalance() {
+        return initialBalance;
+    }
+
+    public void setInitialBalance(double initialBalance) {
+        this.initialBalance = initialBalance;
+    }
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+
 }
