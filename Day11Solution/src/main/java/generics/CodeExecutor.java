6fd4699fda55de8822671550ad920f2a7bd2f184
harikrishna <chandana16554@mail.npu.edu>
2016-12-10 03:11:43 -0800

    Merge branch 'master' of https://github.com/rushidesai1/Java_Training_Nov_2016 into Branch-hk
    # Conflicts:
    #	pom.xml

--- Day11Solution/src/main/java/generics/CodeExecutor.java
+++ Day11Solution/src/main/java/generics/CodeExecutor.java
@@ -3,10 +3,10 @@ package generics;
 /**
  * Created by harik on 12/9/2016.
  */
-public class CodeExecutor implements IMapper<String>  {
+public class CodeExecutor implements IMapper<String> {
 
     public void map(String type) {
-}
+    }
 }
 
 
--- Day11Solution/src/main/java/generics/IMapper.java
+++ Day11Solution/src/main/java/generics/IMapper.java
@@ -3,7 +3,7 @@ package generics;
 /**
  * Created by harik on 12/9/2016.
  */
-public interface IMapper <T> {
-public void map(T type);
+public interface IMapper<T> {
+    public void map(T type);
 
 }
