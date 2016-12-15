99d5c0e1bd3823b03e10841d5c9090acfef4ceac
anitha <anithareddy41@gmail.com>
2016-12-14 19:36:54 -0600

    bhguyhcdrtdetrsd

--- Day11Solution/src/main/java/IntegerClass.java
+++ Day11Solution/src/main/java/IntegerClass.java
@@ -1,9 +1,10 @@
 /**
  * Created by Venkat Ramana on 12/14/2016.
  */
-public class IntegerClass implements IMapper<Integer>{
-    void map(Integer Object){
+public class IntegerClass implements IMapper{
 
+
+    public Object map(Object T) {
+        return null;
     }
-
 }
--- Day11Solution/src/main/java/StringClass.java
+++ Day11Solution/src/main/java/StringClass.java
@@ -1,7 +1,7 @@
 /**
  * Created by Venkat Ramana on 12/14/2016.
  */
-public class StringClass implements IMapper<String> {
+public class StringClass implements IMapper {
 
 
     public Object map(Object T) {
